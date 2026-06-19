f636ec17c42c5c79c6e4be1ce9d5fb4ca6abfd40
Shahma Sharin <sharinshahmaa@gmail.com>
2026-06-19 15:47:06 +0530

    Time: 30 ms (14.21%), Space: 59.3 MB (30.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+   var isAnagram = function(s, t) {
+    return s.split('')
+        .sort()
+        .join('') ===t.split('')
+        .sort().join('');
+}; 
