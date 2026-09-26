ae13817b6903f558cbbf9e1a4415a522a7067dc5
Shahma Sharin <sharinshahmaa@gmail.com>
2026-09-26 09:43:24 +0530

    Time: 1 ms (72.36%), Space: 58.1 MB (27.16%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function singleNumber(nums) {
+    let result = 0;
+    for (let num of nums) {
+        result ^= num; 
+    }
+    return result;
+}
